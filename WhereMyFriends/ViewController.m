3ae40c9b2f2514f85418bd40d6d6e9b3f14fec60
陳育賢 <chenyuhsien@chenyuxiande-MacBook-Pro.local>
2016-08-22 15:31:25 +0800

    新增朋友位置大頭針起點

--- WhereMyFriends/ViewController.m
+++ WhereMyFriends/ViewController.m
@@ -55,19 +55,25 @@
     NSLog(@"現在的緯度是:%f,經度是:%f",lat,lon);
     
     if((self.switchBtn.on == YES)) {
+        //回傳自己位置資料API
+        NSString *urlString=[NSString stringWithFormat:@"http://class.softarts.cc/FindMyFriends/updateUserLocation.php?GroupName=ap103&UserName=YuHsien&Lat=%f&Lon=%f",lat,lon];
+        //取朋友位置的API
+        NSString *urlFriends=[NSString stringWithFormat:@"http://class.softarts.cc/FindMyFriends/queryFriendLocations.php?GroupName=ap103"];
         
-        NSString *urlString=[NSString stringWithFormat:@"http://class.softarts.cc/FindMyFriends/updateUserLocation.php?GroupName=AP103&UserName=YuHsien&Lat=%f&Lon=%f",lat,lon];
-        
-        
+        //回傳自己位置資料API
         NSURL *url = [NSURL URLWithString:urlString];
         NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:60.0];
+        //取朋友位置的API
+        NSURL *url2 = [NSURL URLWithString:urlFriends];
+        NSURLRequest *request2 = [NSURLRequest requestWithURL:url2 cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:60.0];
+        
         
         // Prepare NSURLSession
         NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
         NSURLSession *session = [NSURLSession sessionWithConfiguration:config];
         
         //[_loadingIndicatorView startAnimating];
-        
+        //Part1
         NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
             
             if(error)
@@ -87,7 +93,31 @@
             
         }];
         
+        //從Server裡讀取朋友的位置
+        NSURLSessionDataTask *task2 = [session dataTaskWithRequest:request2 completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+            
+            if(error)
+            {
+                NSLog(@"Error2: %@",error.description);
+            }
+            else
+            {
+                //            NSString *content = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+                //            NSLog(@"JSON: %@",content);
+                
+                // Parse JSON
+                NSDictionary *friDatas = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+                NSLog(@"JSON2: %@",friDatas);
+                NSString * friLat=[friDatas objectForKey:@"lat"];
+                NSString * friLon=[friDatas objectForKey:@"lon"];
+            }
+            
+        }];
+
+        
+        
         [task resume];
+        [task2 resume];
         
     }
     
